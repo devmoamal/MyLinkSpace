707b4cb76de8bd6c0956f69865ee4b5b110c0ca3
Moamal Hussin <dev.moamal@gmail.com>
2026-01-01 20:09:32 +0300

    Add LINK_ICONS and LINK_TYPES constants for link categorization

---
+++ shared/constants/index.ts
@@ -0,0 +1,21 @@
+export const LINK_ICONS = [
+  // Websites
+  "Website", // As defult icon for URLs
+  "Github",
+  "Instagram",
+  "Telegram",
+  "TikTok",
+  "Facebook",
+  "X",
+  "YouTube",
+  "LinkedIn",
+  "Blog",
+  "Portfolio",
+  "Shop",
+
+  // Contact
+  "Email",
+  "Phone",
+] as const;
+
+export const LINK_TYPES = ["Website", "Email", "Phone"] as const;
