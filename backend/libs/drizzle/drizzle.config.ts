6541d8924d4ce6dac624fe8650cd6f658edadfb4
Moamal Hussin <dev.moamal@gmail.com>
2026-01-03 21:53:45 +0300

    feat(backend): adding drizzle config.

--- backend/libs/drizzle/drizzle.config.ts
+++ backend/libs/drizzle/drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/db/schema/index.ts",
+  schema: "./src/db/schema.ts",
   out: "./migrations",
   dialect: "sqlite",
   dbCredentials: {
