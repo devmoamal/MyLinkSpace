ccc41b186bc6b5622d6bf83cf50251cfa5296202
Moamal Hussin <dev.moamal@gmail.com>
2026-01-05 23:07:17 +0300

    clean(middleware): Add todo comments

--- backend/src/middlewares/logger.middleware.ts
+++ backend/src/middlewares/logger.middleware.ts
@@ -1,6 +1,6 @@
 import { logger } from "hono/logger";
 
-// Return a Hono logger middleware
+// TODO: update logger to own logger middleware
 export default function LoggerMiddleware() {
   return logger();
 }
